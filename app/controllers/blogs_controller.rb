37f5caa5584338628c625bf12da4acaea7046f52
yuya yamada <yuya19920721@gmail.com>
2021-05-19 23:52:45 +0900

    文言修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,20 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :set_blog, only: %i[show edit update destroy]
 
   def index
     @blogs = Blog.all
   end
 
   def show
+    
   end
 
   def new
-    if params[:back]
-      @blog = Blog.new(blog_params)
-    else
-      @blog = Blog.new
-    end
+    @blog = if params[:back]
+              Blog.new(blog_params)
+            else
+              Blog.new
+            end
   end
 
   def confirm
@@ -21,14 +22,13 @@ class BlogsController < ApplicationController
     render :new if @blog.invalid?
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @blog = current_user.blogs.build(blog_params)
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: "投稿しました。" }
+        format.html { redirect_to @blog, notice: '投稿しました。' }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +40,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: "更新しました。" }
+        format.html { redirect_to @blog, notice: '更新しました。' }
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "投稿を削除しました。" }
+      format.html { redirect_to blogs_url, notice: '投稿を削除しました。' }
       format.json { head :no_content }
     end
   end
@@ -60,11 +60,10 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-      @blog = Blog.find(params[:id])
+    @blog = Blog.find(params[:id])
   end
 
   def blog_params
-      params.require(:blog).permit(:content, :image, :image_cache, :user_id)
+    params.require(:blog).permit(:content, :image, :image_cache, :user_id)
   end
-
 end
