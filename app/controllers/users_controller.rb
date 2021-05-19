a80d3e9fafe9149324b04c3e447959d3cf286e5c
yuya yamada <yuya19920721@gmail.com>
2021-05-19 16:11:31 +0900

    ユーザーの詳細画面と登録後のリダイレクト設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to blogs_path
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
