83a86dc00bc77b561d7cf93ce1081e60dd76f27e
yuya yamada <yuya19920721@gmail.com>
2021-05-20 00:40:35 +0900

    エラー修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,11 +8,7 @@ class BlogsController < ApplicationController
   def show; end
 
   def new
-    @blog = if params[:back]
-              Blog.new(blog_params)
-            else
-              Blog.new
-            end
+    @blog = Blog.new
   end
 
   def confirm
@@ -34,23 +30,16 @@ class BlogsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: '更新しました。' }
-        format.json { render :show, status: :ok, location: @blog }
+        redirect_to blogs_path, notice: "投稿を編集しました。"
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   def destroy
     @blog.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_url, notice: '投稿を削除しました。' }
-      format.json { head :no_content }
-    end
+    redirect_to blogs_path, notice: "投稿を削除しました。"
   end
 
   private
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
   belongs_to :user
+  validates :image, presence: true
+  validates :content, presence: false
 end
